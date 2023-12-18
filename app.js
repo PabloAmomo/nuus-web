f535a66fe1aad2d7841b995bbd98c3373579bc63
Pablo Amomo <pabloamomo@gmail.com>
2023-12-18 09:53:08 +0100

    .

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
-// const BASE_URL = 'https://feedapi.yatchapp.dev';
-const BASE_URL = 'http://127.0.0.1:8080';
+const BASE_URL = 'https://feedapi.yatchapp.dev';
 
 // Entry point for the application
 const init = () => {
