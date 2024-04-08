73102a526eb64a1ffe77e9bba4808e1a300f9bbd
Pablo amomo <pabloamomo@gmail.com>
2024-04-08 12:58:52 +0200

    add read items in viewport

--- app.js
+++ app.js
@@ -605,9 +605,12 @@ const processItems = (data) => {
     // If no items, add class to body or remove, if exists
     if (!items || items.length === 0) document.body.classList.add('no-items');
     else document.body.classList.remove('no-items');
-    // Set as readed the first 3 items
+    // Set as readed the items in viewport
     const readedToSend = [];
-    items?.slice(0, 3)?.forEach((item) => readedToSend.push(item.id));
+    items.forEach((item) => {
+      let itemEl = document.querySelector(`.list-item [data-id="${item.id}"]`);
+      if (isInViewport(itemEl)) { readedToSend.push(item.id); }
+    });
     if (readedToSend.length > 0) setReaded(readedToSend.join(','));
   } catch (err) {
     errorLog('processItems', err);
@@ -656,6 +659,18 @@ const polyfill = () => {
     };
   }
 };
+
+// Check if elements is in viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  var html = document.documentElement;
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || html.clientHeight) &&
+    rect.right <= (window.innerWidth || html.clientWidth)
+  );
+}
 
 // Constants
 let LAST_READED = null;
